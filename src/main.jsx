57ff7fad1b46a7fddc2080a5ef8101bd52cc4b3d
edgarmuyomba <edgarmatthew247@gmail.com>
2023-12-25 22:28:24 +0300

    mobile responsiveness

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './styles/reset.css'
-import './styles/main.css'
+import App from './App.jsx';
+import './styles/reset.css';
+import './styles/main.css';
+import './styles/mobile.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
